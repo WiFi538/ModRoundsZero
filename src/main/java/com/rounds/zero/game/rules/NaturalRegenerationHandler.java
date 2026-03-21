2111d15b2c37d8222a0c30aedf1efc9163033bdd
Ivan <filippovivan538@mail.ru>
2026-03-21 23:04:00 +0300

    implemented the deactivation of regeneration

--- src/main/java/com/rounds/zero/game/rules/NaturalRegenerationHandler.java
+++ src/main/java/com/rounds/zero/game/rules/NaturalRegenerationHandler.java
@@ -1,4 +1,26 @@
 package com.rounds.zero.game.rules;
 
-public class NaturalRegenerationHandler {
+import net.minecraft.server.network.ServerPlayerEntity;
+
+public final class NaturalRegenerationHandler {
+    private static boolean disabled = true;
+
+    private NaturalRegenerationHandler() {
+    }
+
+    public static boolean isDisabled() {
+        return disabled;
+    }
+
+    public static void setDisabled(boolean disabled) {
+        NaturalRegenerationHandler.disabled = disabled;
+    }
+
+    public static boolean canFoodHeal(ServerPlayerEntity player) {
+        if (disabled) {
+            return false;
+        }
+
+        return player.canFoodHeal();
+    }
 }
