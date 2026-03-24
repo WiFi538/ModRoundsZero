73e7e7a0d18c47419e72f54b93e785526dae721d
Ivan <filippovivan538@mail.ru>
2026-03-24 19:02:46 +0300

    rework Arena.java for 4 team

--- src/main/java/com/rounds/zero/game/arena/Arena.java
+++ src/main/java/com/rounds/zero/game/arena/Arena.java
@@ -6,11 +6,15 @@ public class Arena {
     private final String name;
     private final BlockPos redSpawn;
     private final BlockPos blueSpawn;
+    private final BlockPos greenSpawn;
+    private final BlockPos yellowSpawn;
 
-    public Arena(String name, BlockPos redSpawn, BlockPos blueSpawn) {
+    public Arena(String name, BlockPos redSpawn, BlockPos blueSpawn, BlockPos greenSpawn, BlockPos yellowSpawn) {
         this.name = name;
         this.redSpawn = redSpawn;
         this.blueSpawn = blueSpawn;
+        this.greenSpawn = greenSpawn;
+        this.yellowSpawn = yellowSpawn;
     }
 
     public String getName() {
@@ -23,5 +27,13 @@ public class Arena {
 
     public BlockPos getBlueSpawn() {
         return blueSpawn;
+    }
+
+    public BlockPos getGreenSpawn() {
+        return greenSpawn;
+    }
+
+    public BlockPos getYellowSpawn() {
+        return yellowSpawn;
     }
 }
